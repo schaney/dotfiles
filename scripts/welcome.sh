f51028bc0afb76f30bd3cf58fda66ca8c54926b9
schaney <sean@adzerk.com>
2016-11-16 17:35:59 -0500

    rejigger batman rainbow

--- scripts/welcome.sh
+++ scripts/welcome.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cat $dir/batman.ascii | lolcat --seed 1 -p 8 -F 0.7
+cat $dir/batman.ascii | lolcat --seed 3 -p 10000 -F 0.7
 echo
