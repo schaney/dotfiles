a37e04893cb5c93d832273c4b0b2b683fd3f3104
schaney <sean@adzerk.com>
2021-03-27 00:29:22 -0400

    backup splash

--- dotfiles/scripts/welcome.sh
+++ dotfiles/scripts/welcome.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+batman="$(cat $dir/batman.ascii)"
 if command -v lolcat > /dev/null; then
-  cat $dir/batman.ascii | lolcat --seed 3 -p 10000 -F 0.7
+  echo "$batman" | lolcat --seed 3 -p 10000 -F 0.7
+else
+  echo "$(tput setaf 3)${batman}"
 fi
 echo
