b11171116c5763725bceb9bbdc06e48e74ff0897
schaney <sean@adzerk.com>
2025-06-11 17:36:16 -0400

    asdf

--- dotfiles/scripts/welcome.sh
+++ dotfiles/scripts/welcome.sh
@@ -11,7 +11,7 @@ if command -v lolcat > /dev/null; then
   seed=$(( ($RANDOM % 256) + 1))
   freq=$(echo "scale=3; $(((RANDOM % 200) + 100))/1000" | bc)
   spread=$(echo "scale=3; $(((RANDOM % 1000) + 1))/100 + 0.1" | bc)
-  echo "$picture" | lolcat --seed $seed -F $freq -t -i -p $spread
+  echo "$picture" | lolcat --seed $seed -F $freq -t -p $spread
 else
   color=$(( ($RANDOM % 256) + 1))
   echo "$(tput setaf $color)${picture}"
